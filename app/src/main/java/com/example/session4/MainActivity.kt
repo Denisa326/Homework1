97865c3063d6772d917e4ab0a33f8f50a5ca4fa8
Denisa326 <denisa.iriciuc101@gmail.com>
2026-05-11 17:52:10 +0300

    homework4

--- app/src/main/java/com/example/session4/MainActivity.kt
+++ app/src/main/java/com/example/session4/MainActivity.kt
@@ -30,6 +30,8 @@ import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
